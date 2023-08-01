f19391019c951a45c841316ce4de0a9bb0063566
frenziopen <frenziopen@gmail.com>
2023-07-31 21:31:22 -0700

    solenoid
    something

--- Software/Arduino/test_solenoid/test_solenoid.ino
+++ Software/Arduino/test_solenoid/test_solenoid.ino
@@ -1,26 +1,59 @@
 #include "Arduino.h"
+#include <Wire.h>  
+#include "HT_SH1107Wire.h"
+
+SH1107Wire  display(0x3c, 500000, SDA, SCL ,GEOMETRY_128_64,GPIO10); // addr, freq, sda, scl, resolution, rst
 
 #define power GPIO8 
-#define sol_F GPIO10
-#define sol_R GPIO11
+#define sol_F GPIO11
+#define sol_R GPIO12
+#define temp_IN ADC
+#define sense_POW GPIO9
+#define GPIO16 VBAT_ADC_CTL
 
 void setup() {
+  Serial.begin(9600);
   pinMode(power, OUTPUT);
   pinMode(sol_F, OUTPUT);
   pinMode(sol_R, OUTPUT);
+  pinMode(sense_POW, OUTPUT);
+  pinMode(GPIO16, OUTPUT);
+  
+  display.init();
+  display.clear();
+  display.display();
+  display.setContrast(255);
 
   digitalWrite(sol_F, LOW);
   digitalWrite(sol_R, LOW);
   digitalWrite(power, HIGH);
+  digitalWrite(sense_POW, HIGH);
   delay(1000);
 
 }
 
 void loop() {
-  digitalWrite(sol_F, HIGH);
-  digitalWrite(sol_R, LOW);
+  digitalWrite(GPIO16, HIGH);
+
+  int temp = analogRead(temp_IN);
+  Serial.println(temp);
+  temp *= (5.0/4096.0); //volts
+  temp = (temp - 0.5) * 100.0; //
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.clear();
+  display.display();
+  display.screenRotate(ANGLE_0_DEGREE);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(64, 32-16/2, ("Temp in C: " + String(temp)));
+  display.display();
   delay(1000);
-  digitalWrite(sol_F, LOW);
-  digitalWrite(sol_R, HIGH);
+
+  display.clear();
+  display.display();
+  display.screenRotate(ANGLE_0_DEGREE);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(64, 32-16/2, (String(analogReadmV(temp_IN)*2/1000) + " V"));
+  display.display();
   delay(1000);
+ 
 }
